2d6b177b6d3a347d0f0455ad0ab553214cd8378a
Toby Lam <me@livekn.com>
2015-10-02 09:26:45 +0800

    change to def main() style

--- main.py
+++ main.py
@@ -8,62 +8,66 @@ import re
 import config as c
 config = c.config()
 
-pb = Pushbullet(config.pushbullet_access_token)
-iPhoneList = config.iPhoneList
-iPhone = {}
-triedTimes=0
+def main():
+	pb = Pushbullet(config.pushbullet_access_token)
+	iPhoneList = config.iPhoneList
+	iPhone = {}
+	triedTimes=0
 
-try:
+	try:
 
-	for i in range(0,len(iPhoneList)):
-		parsed = urlparse.urlparse(iPhoneList[i]['reserve_url'])
-		partNumber = urlparse.parse_qs(parsed.query)['partNumber'][0]
-		iPhone[partNumber]={
-			"name":iPhoneList[i]["name"],
-			"reserve_url":iPhoneList[i]["reserve_url"],
-			"pushed":False,
-			"available":False
-		}
+		for i in range(0,len(iPhoneList)):
+			parsed = urlparse.urlparse(iPhoneList[i]['reserve_url'])
+			partNumber = urlparse.parse_qs(parsed.query)['partNumber'][0]
+			iPhone[partNumber]={
+				"name":iPhoneList[i]["name"],
+				"reserve_url":iPhoneList[i]["reserve_url"],
+				"pushed":False,
+				"available":False
+			}
 
-	print("Started, auto update every 10s, stop using ctrl+c")
+		print("Started, auto update every 10s, stop using ctrl+c")
 
-	while (True):
-		result = ""
+		while (True):
+			result = ""
 
-		response = urllib.urlopen(config.availability_json)
-		availability = json.loads(response.read())
+			response = urllib.urlopen(config.availability_json)
+			availability = json.loads(response.read())
 
-		if availability == {}:
-			print("Reserve not available, retry in 20 minutes")
-			time.sleep(1200)
-			continue
+			if availability == {}:
+				print("Reserve not available, retry in 20 minutes")
+				time.sleep(1200)
+				continue
 
-		for partNumber in iPhone:
-			iPhone[partNumber]["available"] = False
+			for partNumber in iPhone:
+				iPhone[partNumber]["available"] = False
 
-		for store in availability:
-			if re.match("R\d{3}",store):
-				productList = availability[store]
-				for item in productList:
-					if item in iPhone:
-						if productList[item] != "NONE":
-							iPhone[item]["available"]=True
-							result = result + iPhone[item]["name"] + " available!\n" + iPhone[item]["reserve_url"] + "\n"
-							if iPhone[item]["pushed"] == False:
-								push = pb.push_link(iPhone[item]["name"] + " available!", iPhone[item]["reserve_url"])
-								iPhone[item]["pushed"]=True
+			for store in availability:
+				if re.match("R\d{3}",store):
+					productList = availability[store]
+					for item in productList:
+						if item in iPhone:
+							if productList[item] != "NONE":
+								iPhone[item]["available"]=True
+								result = result + iPhone[item]["name"] + " available!\n" + iPhone[item]["reserve_url"] + "\n"
+								if iPhone[item]["pushed"] == False:
+									push = pb.push_link(iPhone[item]["name"] + " available!", iPhone[item]["reserve_url"])
+									iPhone[item]["pushed"]=True
 
-		for partNumber in iPhone:
-			if iPhone[partNumber]["available"]==False and iPhone[partNumber]["pushed"]==True:
-				iPhone[partNumber]["pushed"]=False
-				result = result + iPhone[partNumber]["name"] + " gone :(\n"
+			for partNumber in iPhone:
+				if iPhone[partNumber]["available"]==False and iPhone[partNumber]["pushed"]==True:
+					iPhone[partNumber]["pushed"]=False
+					result = result + iPhone[partNumber]["name"] + " gone :(\n"
 
-		if result == "":
-			result = "Not available"
+			if result == "":
+				result = "Not available"
 
-		triedTimes=triedTimes+1
-		print(time.strftime("%Y-%m-%d %H:%M:%S") + " | " + result)
-		time.sleep(10)
+			triedTimes=triedTimes+1
+			print(time.strftime("%Y-%m-%d %H:%M:%S") + " | " + result)
+			time.sleep(10)
 
-except KeyboardInterrupt:
-	print("\nBye")
+	except KeyboardInterrupt:
+		print("\nBye")
+
+if __name__ == "__main__":
+	main()
